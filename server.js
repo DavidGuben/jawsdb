0c70094b3ce51e44508b4a892238eb3836229f0d
DavidGuben <davidjg6@gmail.com>
2016-08-01 19:11:15 -0400

    added jawsdb database

--- server.js
+++ server.js
@@ -8,14 +8,12 @@ var express 	= require('express');
 var bodyParser 	= require('body-parser');
 
 
-
-
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-// Sets up the Express app to handle data parsing 
+// Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
@@ -34,7 +32,7 @@ require("./app/routes/html-routes.js")(app)
 
 
 
-// Starts the server to begin listening 
+// Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function(){
 	console.log('App listening on PORT ' + PORT);
